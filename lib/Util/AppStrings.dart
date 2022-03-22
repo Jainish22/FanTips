76c0e98085f1072a00ac4a3c821cab5ee6da62d0
Naimish Khakhriya <naimishkhakhriya@gmail.com>
2022-03-22 17:50:51 +0530

    App String

--- lib/Util/AppStrings.dart
+++ lib/Util/AppStrings.dart
@@ -4,7 +4,7 @@ class AppString {
   //Home String
   static const fanTips= 'FanTips';
   static const logIn= 'Log in';
-  static const Matchesforyou= 'Matches for you';
+  static const matchesForyou = 'Matches for you';
   static const featuredExperts= 'Featured Experts';
   static const topStories= 'Top Stories';
   static const viewAll= 'View All';
