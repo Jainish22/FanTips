241e37038fe73b5bc1c2a8b1797fad8d7f4ce262
ronak <87222698+ronakgoti4499@users.noreply.github.com>
2022-03-24 19:03:03 +0530

    appbar 1 change

--- lib/Screens/IPL/IPLMatches/IPLMatches.dart
+++ lib/Screens/IPL/IPLMatches/IPLMatches.dart
@@ -14,13 +14,13 @@ class IPLMatches extends StatelessWidget {
           children: const [
             AppSizebox.h10,
             Text('Tomorrow, 15 May',style: TextStyle(fontSize: 16)),AppSizebox.h10,
-            MyContainer9(),AppSizebox.h10,
+             MyContainer1(),AppSizebox.h10,
             Text('Sunday, 16 May',style: TextStyle(fontSize: 16)),AppSizebox.h10,
-            MyContainer10(),AppSizebox.h10,
-            MyContainer10(),AppSizebox.h10,
+            MyContainer4(),AppSizebox.h10,
+            MyContainer4(),AppSizebox.h10,
             Text('Sunday, 16 May',style: TextStyle(fontSize: 16)),AppSizebox.h10,
-            MyContainer10(),AppSizebox.h10,
-            MyContainer10(),AppSizebox.h10,
+            MyContainer4(),AppSizebox.h10,
+            MyContainer4(),AppSizebox.h10,
           ],
         ),
       )
--- lib/Screens/IPL/Squads/Squads.dart
+++ lib/Screens/IPL/Squads/Squads.dart
@@ -3,11 +3,14 @@ import 'package:fantips/Widgets/MyContainer.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
+import '../../../Util/AppColor.dart';
+
 class Squads extends StatelessWidget {
   const Squads({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<String> tem = ["Delhi Capitalls","Chennai Super kings","Royal Challengers Banglore","Mumbai Indians","Rajasthan Royals","kings XI Punjab","Kolkata Knight Riders","Sunrisers Hyderabad"];
     return SafeArea(
         child: SingleChildScrollView(
       child: Column(
@@ -17,10 +20,28 @@ class Squads extends StatelessWidget {
             padding: EdgeInsets.zero,
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
-              itemCount: 10,
+              itemCount: 8,
               itemBuilder: (context, index) => Padding(
                 padding: EdgeInsets.only(top: 0.8.h,left: 3.w,right: 3.w),
-                child: MyContainer8(),
+                child: Container(
+                  height: 7.h,
+                  width: 96.w,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5) ,
+                      color: AppColor.light
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 4.w),
+                    child: Row(
+                      children:  [
+                        CircleAvatar(backgroundImage: AssetImage(""),radius: 18,),
+                        AppSizebox.w10,
+                        Text(tem[index],style: TextStyle(fontSize: 12),)
+
+                      ],
+                    ),
+                  ),
+                )
               ))
         ],
       ),
