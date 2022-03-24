eca2ae0c112a8e704e6672961df4ebc1dda4694e
Naimish Khakhriya <naimishkhakhriya@gmail.com>
2022-03-24 16:49:05 +0530

    Upcoming Screen Complete

--- lib/Screens/Matches/Upcoming/Upcoming.dart
+++ lib/Screens/Matches/Upcoming/Upcoming.dart
@@ -1,4 +1,7 @@
+import 'package:fantips/Util/Sizebox.dart';
+import 'package:fantips/Widgets/MyContainer.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class Upcoming extends StatelessWidget {
   const Upcoming({Key? key}) : super(key: key);
@@ -6,10 +9,20 @@ class Upcoming extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          Text('Tomorrow, 15 May')
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            AppSizebox.h10,
+            Text('Tomorrow, 15 May',style: TextStyle(fontSize: 16)),AppSizebox.h10,
+            MyContainer4(),AppSizebox.h10,
+            Text('Sunday, 16 May',style: TextStyle(fontSize: 16)),AppSizebox.h10,
+            MyContainer5(),AppSizebox.h10,
+            MyContainer5(),AppSizebox.h10,
+            Text('Sunday, 16 May',style: TextStyle(fontSize: 16)),AppSizebox.h10,
+            MyContainer5(),AppSizebox.h10,
+            MyContainer5(),AppSizebox.h10,
+          ],
+        ),
       )
     );
   }
