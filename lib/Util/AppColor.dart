22d978442a0b6bbab6e9065ce80c3f22e4fa1560
Naimish Khakhriya <naimishkhakhriya@gmail.com>
2022-03-25 18:12:41 +0530

    match screen

--- lib/Util/AppColor.dart
+++ lib/Util/AppColor.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 class AppColor{
-
   static const light= Color(0xFF1B1B1B);
   static const white= Color(0xFFFFFFFF);
   static const dBlack = Color(0xf4E4E4E72);
