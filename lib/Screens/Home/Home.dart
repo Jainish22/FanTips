9177bf2eb336d9e17464f2f1f5251e76193ac3fb
Naimish Khakhriya <naimishkhakhriya@gmail.com>
2022-04-14 16:26:49 +0530

    slider

--- lib/Screens/Home/Home.dart
+++ lib/Screens/Home/Home.dart
@@ -1,5 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:fantips/Screens/Home/HomeNews.dart';
+import 'package:fantips/Util/AppColor.dart';
 import 'package:fantips/Util/AppStrings.dart';
 import 'package:fantips/Util/Sizebox.dart';
 import 'package:fantips/Widgets/MyAppbar.dart';
@@ -7,86 +7,200 @@ import 'package:fantips/Widgets/MyContainer.dart';
 import 'package:fantips/Widgets/MyTitle.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:sizer/sizer.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+import 'News/Controller/News_Controller.dart';
+
+final List imgList = [MyContainer1(), MyContainer1(), MyContainer1(), MyContainer1()];
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final NewsController _NewsController = Get.put(NewsController());
 
   @override
   Widget build(BuildContext context) {
+    var dtf = DateFormat('dd, yyyy MMM');
+    var day = DateFormat('EEEE');
+    getTime(var time) {
+      return DateFormat.jm().format(DateFormat("hh:mm:ss").parse(time));
+    }
+
+    int _current = 0;
+    final CarouselController _controller = CarouselController();
+
     return Scaffold(
       appBar: MyAppbar(AppString.fanTips, AppString.logIn),
       bottomNavigationBar: MyBottom(),
       body: SafeArea(
         child: ListView(
-           children: [
-             Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 AppSizebox.h15,
-                 const MyTitle(text: AppString.matchesForyou),
-                 AppSizebox.h15,
-                 SizedBox(
-                   height: 19.h,
-                   width: 100.w,
-                   child: CarouselSlider.builder(
-                    options: CarouselOptions(
-                       enableInfiniteScroll: true,
-                       reverse: false,
-                       autoPlay: false,
-                       viewportFraction: 1,
-                     ),
-                     itemCount: 5,
-                     itemBuilder: (BuildContext context, int index, int pageViewIndex){
-                       return const MyContainer1();
-                     }
-                   ),
-                 ),
-                 AppSizebox.h15,
-                 const MyTitle(text: AppString.featuredExperts),
-                 AppSizebox.h15,
-                 SizedBox(
-                   height: 20.h,
-                   width: 100.w,
-                   child: CarouselSlider.builder(
-                       options: CarouselOptions(
-                           enableInfiniteScroll: true,
-                           reverse: false,
-                           autoPlay: false,
-                           viewportFraction: 1
-                       ),
-                       itemCount: 5,
-                       itemBuilder: (BuildContext context, int index, int pageViewIndex){
-                         return const MyContainer2();
-                       }
-                   ),
-                 ),
-                 AppSizebox.h15,
-                 Row(
-                   children: [
-                     const MyTitle(text: AppString.topStories),
-                     Expanded(child: Container()),
-                     const InkWell(child: Text(AppString.viewAll,style: TextStyle(fontSize: 12))),
-                     const Icon(Icons.arrow_forward_ios_sharp,color: Colors.white,size: 12),
-                     AppSizebox.w15
-                   ],
-                 ),
-                 AppSizebox.h15,
-                 SizedBox(
-                   child: Expanded(
-                     child: ListView.builder(
-                       itemCount: 6,
-                       shrinkWrap: true,
-                       physics: const NeverScrollableScrollPhysics(),
-                       itemBuilder: (context, index){
-                         return  InkWell(child: const MyContainer3(),onTap: (){Get.toNamed('news');},);
-                       }
-                     ),
-                   ),
-                 ),
-               ],
-             ),
-           ],
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AppSizebox.h15,
+                const MyTitle(text: AppString.matchesForyou),
+                AppSizebox.h15,
+                SizedBox(
+                  height: 19.h,
+                  width: 100.w,
+                  child: CarouselSlider.builder(
+                      carouselController: _controller,
+                      options: CarouselOptions(
+                        enableInfiniteScroll: true,
+                        reverse: false,
+                        autoPlay: false,
+                        viewportFraction: 1,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _current = index;
+                          });
+                        },
+                      ),
+                      itemCount: 4,
+                      itemBuilder: (BuildContext context, int index, int pageViewIndex) {
+                        return const MyContainer1();
+                      }),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: imgList.asMap().entries.map((entry) {
+                    return GestureDetector(
+                      onTap: () => _controller.animateToPage(entry.key),
+                      child: Container(
+                        width: 14.0,
+                        height: 4.0,
+                        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(15),
+                            color: _current == entry.key ? AppColor.green : AppColor.grey.withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                AppSizebox.h15,
+                const MyTitle(text: AppString.featuredExperts),
+                AppSizebox.h15,
+                SizedBox(
+                  height: 20.h,
+                  width: 100.w,
+                  child: CarouselSlider.builder(
+                      carouselController: _controller,
+                      options: CarouselOptions(
+                        enableInfiniteScroll: true,
+                        reverse: false,
+                        autoPlay: false,
+                        viewportFraction: 1,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _current = index;
+                          });
+                        },
+                      ),
+                      itemCount: 4,
+                      itemBuilder: (BuildContext context, int index, int pageViewIndex) {
+                        return const MyContainer2();
+                      }),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: imgList.asMap().entries.map((entry) {
+                    return GestureDetector(
+                      onTap: () => _controller.animateToPage(entry.key),
+                      child: Container(
+                        width: 14.0,
+                        height: 4.0,
+                        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(15),
+                            color: _current == entry.key ? AppColor.green : AppColor.grey.withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                AppSizebox.h15,
+                Row(
+                  children: [
+                    const MyTitle(text: AppString.topStories),
+                    Expanded(child: Container()),
+                    const InkWell(child: Text(AppString.viewAll, style: TextStyle(fontSize: 12))),
+                    const Icon(Icons.arrow_forward_ios_sharp, color: Colors.white, size: 12),
+                    AppSizebox.w15
+                  ],
+                ),
+                AppSizebox.h15,
+                SizedBox(
+                  child: Expanded(
+                    child: ListView.builder(
+                        itemCount: 5, //_NewsController.getNews.value.news!.length,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 4.w, right: 4.w),
+                              child: Container(
+                                  height: 34.h,
+                                  width: 92.w,
+                                  margin: EdgeInsets.only(bottom: 10),
+                                  decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(5)),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(20),
+                                            image: DecorationImage(
+                                                image: NetworkImage(_NewsController.getNews.value.news![index].image.toString()), fit: BoxFit.cover)),
+                                        height: 20.h,
+                                        width: 90.w,
+                                        child: Image.network(_NewsController.getNews.value.news![index].image.toString(), fit: BoxFit.cover),
+                                      ),
+                                      Column(
+                                        children: [
+                                          Container(
+                                            height: 12.h,
+                                            padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                Text(_NewsController.getNews.value.news![index].title.toString(),
+                                                    maxLines: 1, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                                                Text(_NewsController.getNews.value.news![index].smallDesc ?? "".toString(),
+                                                    style: TextStyle(color: AppColor.grey, fontSize: 12)),
+                                                Text(_NewsController.getNews.value.news![index].time.toString(),
+                                                    style: TextStyle(color: AppColor.grey, fontSize: 12)),
+                                              ],
+                                            ),
+                                          ),
+                                          AppSizebox.h10,
+                                          const Divider(
+                                            height: 2,
+                                            color: AppColor.DividerColor,
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  )),
+                            ),
+                            onTap: () {
+                              Get.toNamed('news');
+                            },
+                          );
+                        }),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/Util/GetPages.dart
+++ lib/Util/GetPages.dart
@@ -1,7 +1,7 @@
 import 'package:fantips/Screens/Experts/ExperInfo/Expertinfo.dart';
 import 'package:fantips/Screens/Experts/Experts.dart';
 import 'package:fantips/Screens/Home/Home.dart';
-import 'package:fantips/Screens/Home/HomeNews.dart';
+import 'package:fantips/Screens/Home/News/HomeNews.dart';
 import 'package:fantips/Screens/IPL/Ipl.dart';
 import 'package:fantips/Screens/Matches/Matches.dart';
 import 'package:fantips/Screens/Matches/Upcoming/UpcomingMatches/UpcomingMatches.dart';
@@ -12,8 +12,8 @@ import '../Screens/More/More.dart';
 
 class Routes {
   static final routes = [
-    GetPage(name: '/home', page: () => const Home()),
-    GetPage(name: '/news', page: () => const HomeNews()),
+    GetPage(name: '/home', page: () => Home()),
+    GetPage(name: '/news', page: () => HomeNews()),
     GetPage(name: '/matches', page: () => const Matches()),
     GetPage(name: '/ipl', page: () => const Ipl()),
     GetPage(name: '/expert', page: () => const Experts()),
