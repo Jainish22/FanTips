278cc0881b13a46e40f3fa02587d9ed91a51acee
Naimish Khakhriya <naimishkhakhriya@gmail.com>
2022-04-27 11:50:06 +0530

    # Pull request

--- lib/Screens/Experts/search.dart
+++ lib/Screens/Experts/search.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
-import 'package:get/state_manager.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../Widgets/MyAppbar.dart';
@@ -13,43 +12,39 @@ class Search extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       bottomNavigationBar: MyBottom(),
       body: Stack(
         children: [
           Padding(
-            padding: EdgeInsets.only(top:6.h,left: 4.w,right: 4.w),
+            padding: EdgeInsets.only(top: 6.h, left: 4.w, right: 4.w),
             child: Container(
               height: 5.h,
               width: 92.w,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                   color: AppColor.light
-              ),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: AppColor.light),
               child: TextFormField(
                 cursorColor: AppColor.white,
-                decoration:  InputDecoration(
+                decoration: InputDecoration(
                   prefixIcon: InkWell(
                     child: Icon(
                       Icons.arrow_back,
                       color: AppColor.white,
                     ),
-                    onTap: (){Get.back();},
+                    onTap: () {
+                      Get.back();
+                    },
                   ),
                   hintText: "search",
                   hintStyle: TextStyle(color: Colors.white),
                   border: InputBorder.none,
-
                 ),
               ),
             ),
           ),
-          Center(child: Column(
+          Center(
+              child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("asset/player/Placeholder.png"),
-              Text("No Data!")
-            ],
+            children: [Image.asset("asset/player/Placeholder.png"), Text("No Data!")],
           ))
         ],
       ),
